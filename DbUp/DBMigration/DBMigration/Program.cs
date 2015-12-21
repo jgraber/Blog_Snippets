fb33cacd841801594fcc017c763e5f781b8a2a9a
Johnny Graber <jg@jgraber.ch>
2015-12-21 21:50:22 +0100

    add basic configuration for DbUp

--- DbUp/DBMigration/DBMigration/Program.cs
+++ DbUp/DBMigration/DBMigration/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using DbUp;
 
 namespace DBMigration
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            var connectionString = ConfigurationManager.ConnectionStrings["BlogSnippets"].ConnectionString;
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
         }
     }
 }
