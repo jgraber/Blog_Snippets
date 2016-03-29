2c528b9455dccbc3b128bed9c0f619b48f6d0df8
Johnny Graber <jg@jgraber.ch>
2016-03-29 23:32:46 +0200

    add example scripts for German umlauts and other special chars

---
+++ DbUp/DBMigration/DBMigration/Scripts/Script0002 - Start Unicode Example.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE UnicodeExample
+(
+	Id	    INT IDENTITY(1,1),
+	German	NVARCHAR(255),
+	French	NVARCHAR(255),
+	Spanish	NVARCHAR(255),
+	CONSTRAINT pk_UnicodeExample_Id PRIMARY KEY (Id),
+);
