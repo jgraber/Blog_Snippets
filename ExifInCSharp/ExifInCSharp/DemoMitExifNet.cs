f9fe255d5f51d1e820bd97f7d8a3f8ff3360976a
Johnny Graber <jg@jgraber.ch>
2016-04-02 14:18:43 +0200

    Using um File.Open, damit dies wieder freigegeben wird

--- ExifInCSharp/ExifInCSharp/DemoMitExifNet.cs
+++ ExifInCSharp/ExifInCSharp/DemoMitExifNet.cs
@@ -16,28 +16,30 @@ namespace ExifInCSharp
     {
         public void ExtrahiereGPSInformationen(string filePath)
         {
-            var s = File.Open(filePath, FileMode.Open);
-            var image = new Bitmap(s);
-            var exif = new Exif(image.PropertyItems);
+            using (var s = File.Open(filePath, FileMode.Open))
+            {
+                var image = new Bitmap(s);
+                var exif = new Exif(image.PropertyItems);
 
-            Console.WriteLine($"Hersteller: {exif.Make}");
-            Console.WriteLine($"Kamera: {exif.Model}");
+                Console.WriteLine($"Hersteller: {exif.Make}");
+                Console.WriteLine($"Kamera: {exif.Model}");
 
+                //var type = exif.GetType();
+                //var properties = type.GetProperties().OrderBy(p => p.Name).ToArray();
+                //foreach (var property in properties)
+                //{
+                //    var val = property.GetValue(exif, null);
+                //    if (val == null)
+                //    {
+                //        Console.WriteLine(property.Name + ": N/A");
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine(property.Name + ": " + val);
+                //    }
+                //}
+            }
 
-            //var type = exif.GetType();
-            //var properties = type.GetProperties().OrderBy(p => p.Name).ToArray();
-            //foreach (var property in properties)
-            //{
-            //    var val = property.GetValue(exif, null);
-            //    if (val == null)
-            //    {
-            //        Console.WriteLine(property.Name + ": N/A");
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine(property.Name + ": " + val);
-            //    }
-            //}
         }
     }
 }
