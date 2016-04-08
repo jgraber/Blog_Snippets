0283db6772ef32a980a7bbcd04d930ed1b879028
Johnny Graber <Johnny.Graber@FMH.ch>
2016-04-08 16:43:38 +0200

    Beispiel für Humanizr eingefügt

---
+++ HumanizrDemo/HumanizrDemo/HumanizrExamples.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Humanizer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HumanizrDemo
+{
+    [TestClass]
+    public class HumanizrExamples
+    {
+        [TestMethod]
+        public void Text_KuerzenAufBestimmteLaenge_RestMitPunktenAngedeutet()
+        {
+            var meinText = "Dies ist ein sehr langer Text";
+
+            var result = meinText.Truncate(5);
+
+            Assert.AreEqual("Dies…", result);
+        }
+    }
+}
