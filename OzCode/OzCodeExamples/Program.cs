bf7415b562661f532f72d47f57baf6a360c6c7d9
Johnny Graber <jg@jgraber.ch>
2016-10-24 22:27:04 +0200

    add examples for OzCode demo

---
+++ OzCode/OzCodeExamples/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OzCodeExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var demo = new Demo();
+            //var result = demo.GetFromDB();
+            //demo.DoAThing();
+
+
+            var i = 5;
+            var text = "This is a long string";
+
+            if (text.Length == i)
+            {
+                Console.WriteLine("Hello");
+            }
+            else if (text.Contains("9") || text.Contains("is a"))
+            {
+                Console.WriteLine(text);
+            }
+            else
+            {
+                Console.WriteLine("not found");
+            }
+
+        }
+    }
+
+    public class ComplexObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Place> Places { get; set; }
+    }
+
+    public class Place
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+    }
+
+    class Demo
+    {
+
+        public List<ComplexObject> GetFromDB()
+        {
+            var result = new List<ComplexObject>();
+            result.Add(new ComplexObject()
+            {
+                Id = 1,
+                Name = "US East",
+                Places = new List<Place>()
+                {
+                    new Place() {City = "New York"},
+                    new Place() {City = "Boston"},
+                    new Place() {City = "New Haven"},
+                    new Place() {City = "Providence"}
+                }
+            });
+            result.Add(new ComplexObject()
+            {
+                Id = 2,
+                Name = "Ireland",
+                Places = new List<Place>()
+                {
+                    new Place() {City = "Dublin"},
+                    new Place() {City = "Cork"},
+                    new Place() {City = "Limerick"},
+                    new Place() {City = "Galway"}
+                }
+            });
+
+            return result;
+        }
+
+        public void DoAThing()
+        {
+            try
+            {
+                AnotherThing();
+            }
+            catch (Exception ex)
+            {
+
+                throw new BadImageFormatException("Export not possible", ex);
+            }
+        }
+
+        private void AnotherThing()
+        {
+            try
+            {
+                TheStartOfTheProblem("a");
+            }
+            catch (Exception ex)
+            {
+                throw new ArithmeticException("parameter a is wrong", ex);
+            }
+        }
+
+        private void TheStartOfTheProblem(string input)
+        {
+            throw new ArgumentNullException(nameof(input));
+        }
+    }
+}
---
+++ OzCode/OzCodeExamples/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OzCodeExamples")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OzCodeExamples")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d6a6b084-0635-44d3-a59e-10bf65ff790e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
