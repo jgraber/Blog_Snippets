196590cb8bfaf2d1bd165fb634ebd90a4e0792e1
Johnny Graber <Johnny.Graber@FMH.ch>
2017-07-03 11:31:29 +0200

    add code to send emails

--- SendEmails/SendEmails/Program.cs
+++ SendEmails/SendEmails/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SendEmails
 {
@@ -8,6 +11,20 @@ namespace SendEmails
     {
         static void Main(string[] args)
         {
+            var toAddress = "somewhere@mailinator.com";
+            var fromAddress = "you@host.com";
+            var message = "your message goes here";
+
+            // create the email
+            var mailMessage = new MailMessage();
+            mailMessage.To.Add(toAddress);
+            mailMessage.Subject = "Sending emails is easy";
+            mailMessage.From = new MailAddress(fromAddress);
+            mailMessage.Body = message;
+
+            // send it (settings in web.config / app.config) 
+            var smtp = new SmtpClient();
+            smtp.Send(mailMessage);
         }
     }
 }
