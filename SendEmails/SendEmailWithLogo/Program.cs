3a0636baaadb13c028e7cdb116912efe0bf3515e
Johnny Graber <Johnny.Graber@FMH.ch>
2017-07-05 11:34:30 +0200

    Copy code for sending emails

---
+++ SendEmails/SendEmailWithLogo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendEmailWithLogo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var toAddress = "somewhere@mailinator.com";
+            var fromAddress = "you@host.com";
+            var message = "your message goes here";
+            // create the email
+            var mailMessage = new MailMessage();
+            mailMessage.To.Add(toAddress);
+            mailMessage.Subject = "Sending emails is easy";
+            mailMessage.From = new MailAddress(fromAddress);
+
+            mailMessage.Body = message;
+
+            // send it (settings in web.config / app.config) 
+            var smtp = new SmtpClient();
+            smtp.Send(mailMessage);
+        }
+    }
+}
