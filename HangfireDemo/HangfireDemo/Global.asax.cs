a3892eaf9eb24a56feae9154e64f7c1086613010
Johnny Graber <Johnny.Graber@FMH.ch>
2017-07-25 07:56:37 +0200

    add Serilog

--- HangfireDemo/HangfireDemo/Global.asax.cs
+++ HangfireDemo/HangfireDemo/Global.asax.cs
@@ -8,10 +8,17 @@ using System.Web.Routing;
 
 namespace HangfireDemo
 {
+    using Serilog;
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+
+            var log =
+                new LoggerConfiguration().ReadFrom.AppSettings()
+                    .CreateLogger();
+            Log.Logger = log;
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
