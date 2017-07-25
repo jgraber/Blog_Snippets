bf5cd7b431bc7863cd58c95cde9731113522b5dc
Johnny Graber <Johnny.Graber@FMH.ch>
2017-07-25 17:14:06 +0200

    Add console application for background processing

---
+++ HangfireDemo/JobProcessor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobProcessor
+{
+    using Hangfire;
+
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            GlobalConfiguration.Configuration.UseSqlServerStorage("HangfireDB");
+
+            Console.WriteLine("Ready?");
+            Console.ReadKey();
+
+            using (var server = new BackgroundJobServer())
+            {
+                Console.WriteLine("Hangfire Server started. Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
