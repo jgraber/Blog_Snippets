e79e019fbb053641fa0783a0204f4e44ea866e40
Johnny Graber <Johnny.Graber@FMH.ch>
2017-07-31 18:05:26 +0200

    read Xml from file and deserialize to object

--- PasteSpecial/PasteSpecial/Program.cs
+++ PasteSpecial/PasteSpecial/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace PasteSpecial
 {
+    using System.IO;
+    using System.Xml.Serialization;
+
+    using PasteSpecial.Pasted;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var pathToXmlFile = @"Source\CT007.xml";
+
+            CT007 result = new CT007();
+            XmlSerializer serializer = new XmlSerializer(typeof(CT007));
+
+            using (FileStream fileStream = new FileStream(pathToXmlFile, FileMode.Open))
+            {
+                result = (CT007)serializer.Deserialize(fileStream);
+            }
         }
     }
 }
