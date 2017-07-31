9b7950c6839723fd30fbd3cf991630ab03f5b6a9
Johnny Graber <Johnny.Graber@FMH.ch>
2017-07-31 17:42:31 +0200

    Output of Paste Special

---
+++ PasteSpecial/PasteSpecial/Pasted/CT007.cs
@@ -0,0 +1,164 @@
+﻿namespace PasteSpecial.Pasted
+{
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class CT007
+    {
+
+        private string titleIsMandatoryField;
+
+        private string fileIsMandatoryField;
+
+        private string placeIsMandatoryField;
+
+        private string creditsIsMandatoryField;
+
+        private string dateIsMandatoryField;
+
+        private string titleDisplayField;
+
+        private string fileDisplayField;
+
+        private string placeDisplayField;
+
+        private string creditsDisplayField;
+
+        private string dateDisplayField;
+
+        /// <remarks/>
+        public string TitleIsMandatory
+        {
+            get
+            {
+                return this.titleIsMandatoryField;
+            }
+            set
+            {
+                this.titleIsMandatoryField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string FileIsMandatory
+        {
+            get
+            {
+                return this.fileIsMandatoryField;
+            }
+            set
+            {
+                this.fileIsMandatoryField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string PlaceIsMandatory
+        {
+            get
+            {
+                return this.placeIsMandatoryField;
+            }
+            set
+            {
+                this.placeIsMandatoryField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string CreditsIsMandatory
+        {
+            get
+            {
+                return this.creditsIsMandatoryField;
+            }
+            set
+            {
+                this.creditsIsMandatoryField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string DateIsMandatory
+        {
+            get
+            {
+                return this.dateIsMandatoryField;
+            }
+            set
+            {
+                this.dateIsMandatoryField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string TitleDisplay
+        {
+            get
+            {
+                return this.titleDisplayField;
+            }
+            set
+            {
+                this.titleDisplayField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string FileDisplay
+        {
+            get
+            {
+                return this.fileDisplayField;
+            }
+            set
+            {
+                this.fileDisplayField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string PlaceDisplay
+        {
+            get
+            {
+                return this.placeDisplayField;
+            }
+            set
+            {
+                this.placeDisplayField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string CreditsDisplay
+        {
+            get
+            {
+                return this.creditsDisplayField;
+            }
+            set
+            {
+                this.creditsDisplayField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string DateDisplay
+        {
+            get
+            {
+                return this.dateDisplayField;
+            }
+            set
+            {
+                this.dateDisplayField = value;
+            }
+        }
+    }
+
+
+}
