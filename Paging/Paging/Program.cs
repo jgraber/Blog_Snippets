aaf7438a85beda6b0e0dc65ce3718e5f9ff4b8d9
Johnny Graber <jg@jgraber.ch>
2017-09-09 18:11:42 +0200

    add code for accessing the db

--- Paging/Paging/Program.cs
+++ Paging/Paging/Program.cs
@@ -6,10 +6,68 @@ using System.Threading.Tasks;
 
 namespace Paging
 {
+    using System.Configuration;
+    using System.Data;
+    using System.Data.SqlClient;
+
+    using Dapper;
+
     public class Program
     {
         static void Main(string[] args)
         {
+
+            var connectionString = ConfigurationManager.ConnectionStrings["db"].ConnectionString;
+            var conn = new SqlConnection(connectionString);
+
+            var selectQuery = @"SELECT [Id]
+                                      ,[FirstName]
+                                      ,[LastName]
+                                      ,[Email]
+                                FROM [Users]
+                                WHERE [Subscribed] = 1
+                                Order By [Id]
+                                OFFSET @offset ROWS
+                                FETCH NEXT @rowsPerPage ROWS ONLY;";
+
+
+            ReadWithSqlCommand(connectionString, selectQuery);
+            Console.WriteLine("----------------");
+            ReadWithDapper(conn, selectQuery);
+        }
+
+        private static void ReadWithSqlCommand(string connectionString, string selectQuery)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(selectQuery, connection);
+
+                SqlParameter offset = new SqlParameter("@offset", SqlDbType.Int, 0);
+                offset.Value = 0;
+                command.Parameters.Add(offset);
+
+                SqlParameter rowsPerPage = new SqlParameter("@rowsPerPage", SqlDbType.Int, 0);
+                rowsPerPage.Value = 20;
+                command.Parameters.Add(rowsPerPage);
+
+                command.Prepare();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader[0]} {reader[1]} {reader[2]} {reader[3]}");
+                }
+            }
+        }
+
+        private static void ReadWithDapper(SqlConnection conn, string selectQuery)
+        {
+            var results = conn.Query<User>(selectQuery, new { offset = 20, rowsPerPage = 20 }).ToList();
+
+            foreach (var user in results)
+            {
+                Console.WriteLine($"{user.Id} {user.FirstName} {user.LastName} {user.Email}");
+            }
         }
     }
 }
