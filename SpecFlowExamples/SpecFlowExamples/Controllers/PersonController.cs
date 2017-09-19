16adb0123858faa9a365f656a2898bf3d06545d6
Johnny Graber <jg@jgraber.ch>
2017-09-19 21:09:42 +0200

    Create web app for Specflow tests

---
+++ SpecFlowExamples/SpecFlowExamples/Controllers/PersonController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpecFlowExamples.Controllers
+{
+    using SpecFlowExamples.Models;
+
+    public class PersonController : Controller
+    {
+        private static List<Person> people;
+
+        public PersonController()
+        {
+            people = new List<Person>();
+        }
+
+        // GET: Person
+        public ActionResult Index()
+        {
+            var newPerson = (Person)TempData["newPerson"];
+            if (newPerson != null)
+            {
+                ViewBag.TheResult = newPerson;
+                ViewBag.Message = $"{newPerson.FirstName} {newPerson.LastName} successful created";
+            }
+
+            return View(people);
+        }
+
+        // GET: Person/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Person/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Person/Create
+        [HttpPost]
+        public ActionResult Create(Person person)
+        {
+            try
+            {
+                people.Add(person);
+                TempData["newPerson"] = person;
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Person/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Person/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Person/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Person/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SpecFlowExamples/SpecFlowExamples/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpecFlowExamples.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
