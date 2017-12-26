fb368a3b96dd3706fe06ddd5d48aaf95ba61a2d5
Johnny Graber <jg@jgraber.ch>
2017-12-26 15:27:03 +0100

    add example with minimal SpecFlow+ config

---
+++ SpecFlowRevisited/Specification/Specification/Examples/ImperativeSteps.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Specification.Examples
+{
+    using NUnit.Framework;
+
+    [Binding]
+    public class ImperativeSteps
+    {
+        [Given(@"I am on the application start screen")]
+        public void GivenIAmOnTheApplicationStartScreen()
+        {
+            Assert.AreEqual(3,3);
+        }
+        
+        [Given(@"I enter a first name of Sherlock")]
+        public void GivenIEnterAFirstNameOfSherlock()
+        {
+            Assert.IsTrue(true);
+        }
+        
+        [Given(@"I enter a last name of Holmes")]
+        public void GivenIEnterALastNameOfHolmes()
+        {
+            Assert.IsFalse(false);
+        }
+        
+        [When(@"I submit my form")]
+        public void WhenISubmitMyForm()
+        {
+            
+        }
+        
+        [Then(@"I should see the application complete confirmation for Sherlock")]
+        public void ThenIShouldSeeTheApplicationCompleteConfirmationForSherlock()
+        {
+           Assert.AreNotEqual(3,4);
+        }
+    }
+}
