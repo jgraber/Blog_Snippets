a60f9780a6a523171b9c0d293f7b80a7e4f4e8a3
Johnny Graber <Johnny.Graber@FMH.ch>
2018-04-24 17:47:50 +0200

    Examples for GraphViz4Net

---
+++ Graphs/Graphs/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace Graphs
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Email})";
+        }
+    }
+}
---
+++ Graphs/Graphs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    using System.IO;
+
+    using Graphviz4Net.Dot;
+    using Graphviz4Net.Dot.AntlrParser;
+    using Graphviz4Net.Graphs;
+
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var graph = new Graph<Person>();
+            var a = new Person(){Name = "AAAAA"};
+            var b = new Person() { Name = "BB BB" };
+
+            graph.AddVertex(a);
+            graph.AddVertex(b);
+            graph.AddEdge(new Edge<Person>(a, b){Label = "reports to", Attributes = { new KeyValuePair<string, string>("color", "0.650 0.200 1.000"), new KeyValuePair<string, string>("size", "15") } });
+
+            Console.WriteLine(graph.ToString());
+
+            var writer = new StringWriter();
+            new GraphToDotConverter().Convert(writer, graph, new AttributesProvider());
+            var result = writer.GetStringBuilder().ToString().Trim();
+
+            Console.WriteLine(result);
+            
+        }
+    }
+
+    public class AttributesProvider : IAttributesProvider
+    {
+        public IDictionary<string, string> GetVertexAttributes(object vertex)
+        {
+            return new Dictionary<string, string>();
+        }
+    }
+}
