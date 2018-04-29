f197dfd08f26eb7664dc507ae5803d4653d25fdf
Johnny Graber <Johnny.Graber@FMH.ch>
2018-04-29 22:08:19 +0200

    add examples for combining PDF files

---
+++ CombinePDF/CombinePDF.Tests/PdfHelperTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace CombinePDF.Tests
+{
+    public class PdfHelperTests
+    {
+        [Fact]
+        public void CountPages_FileWithOnePage_Returns1()
+        {
+            var filePath = @"TestData\first.pdf";
+            var testee = new PdfHelper();
+
+            var result = testee.CountPages(filePath);
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void CombineFiles_OneInputFile_OneResultFile()
+        {
+            var outputPath = "singleFile.pdf";
+            var firstFile = File.OpenRead(@"TestData\first.pdf");
+            var filesToCombine = new List<Stream>{firstFile};
+
+            var testee = new PdfHelper();
+            testee.CombineFiles(filesToCombine, outputPath);
+            
+            Assert.Equal(1, testee.CountPages(outputPath));
+        }
+
+
+        [Fact]
+        public void CombineFiles_2InputFiles_AreCombinedIntoOne()
+        {
+            var outputPath = "combinedFiles.pdf";
+            var firstFile = File.OpenRead(@"TestData\first.pdf");
+            var secondFile = File.OpenRead(@"TestData\second.pdf");
+
+            var filesToCombine = new List<Stream> { firstFile, secondFile };
+
+            var testee = new PdfHelper();
+            testee.CombineFiles(filesToCombine, outputPath);
+
+            Assert.Equal(2, testee.CountPages(outputPath));
+        }
+
+        [Fact]
+        public void CombineFiles_WithMultiplePages_AllPagesAreCombined()
+        {
+            var outputPath = "multiFiles.pdf";
+            var firstFile = File.OpenRead(@"TestData\first.pdf");
+            var thirdFile = File.OpenRead(@"TestData\third.pdf");
+
+            var filesToCombine = new List<Stream> { thirdFile, firstFile };
+
+            var testee = new PdfHelper();
+            testee.CombineFiles(filesToCombine, outputPath);
+
+            Assert.Equal(4, testee.CountPages(outputPath));
+        }
+    }
+}
---
+++ CombinePDF/CombinePDF/PdfHelper.cs
@@ -0,0 +1,44 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CombinePDF
+{
+    public class PdfHelper
+    {
+        public void CombineFiles(List<Stream> files, string outputFilePath)
+        {
+            // the new file to store the combined documents
+            PdfDocument outputDocument = new PdfDocument();
+
+            foreach (Stream file in files)
+            {
+                // Attention: must be in Import mode
+                PdfDocument inputDocument = PdfReader.Open(file, PdfDocumentOpenMode.Import);
+
+                int totalPages = inputDocument.PageCount;
+                for (int pageNumber = 0; pageNumber < totalPages; pageNumber++)
+                {
+                    // Get the page from the input document...
+                    PdfPage page = inputDocument.Pages[pageNumber];
+
+                    // ...and copy it to the output document.
+                    outputDocument.AddPage(page);
+                }
+            }
+
+            // Save the document
+            outputDocument.Save(outputFilePath);
+        }
+
+        public int CountPages(string filePath)
+        {
+            PdfDocument inputDocument = PdfReader.Open(filePath);
+            var pages = inputDocument.PageCount;
+            inputDocument.Close();
+
+            return pages;
+        }
+    }
+}
