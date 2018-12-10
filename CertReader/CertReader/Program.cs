b50b8d84f17ffdf6601f44489ea459c3087e7782
Johnny Graber <Johnny.Graber@FMH.ch>
2018-12-10 21:34:00 +0100

    Example for reading certificates in Windows

---
+++ CertReader/CertReader/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CertReader
+{
+    using System;
+    using System.Security.Cryptography.X509Certificates;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var str = "test.com";
+
+            var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.OpenExistingOnly);
+
+            var collection = store.Certificates.Find(X509FindType.FindBySubjectName, str, false);
+
+            store.Close();
+
+            if (collection.Count == 1)
+            {
+                var myCert = new X509Certificate2(collection[0]);
+                Console.WriteLine($"Certificate '{myCert.FriendlyName}' is found");
+                Console.WriteLine($"Has private key? {myCert.HasPrivateKey}");
+                Console.WriteLine($"Private key: {myCert.PrivateKey.ToXmlString(true)}");
+            }
+            else
+            {
+                Console.WriteLine("Certificate {0} is not found!!", str);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
