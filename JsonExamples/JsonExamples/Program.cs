6e26ba4c620c6c8fb47c648b5eadba041c91dacb
Johnny Graber <Johnny.Graber@fmh.ch>
2019-02-25 21:05:06 +0100

    add example for JSON.net

---
+++ JsonExamples/JsonExamples/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Formatting = Newtonsoft.Json.Formatting;
+
+namespace JsonExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new BlogPost
+            {
+                Id = 1,
+                State = State.Published,
+                PublishedAt = DateTime.Now,
+                Title = "How to use JSON.net"
+            };
+            post.Tags.Add("C#");
+            post.Tags.Add("JSON");
+
+            string output = JsonConvert.SerializeObject(post, Formatting.Indented);
+
+            Console.WriteLine(output);
+
+            var deserializedPost = JsonConvert.DeserializeObject<BlogPost>(output);
+
+            var post2 = new BlogPost
+            {
+                Id = 2,
+                State = State.Draft,
+                PublishedAt = null,
+                Title = "More use cases for JSON.net"
+            };
+
+            var post3 = new BlogPost
+            {
+                Id = 3,
+                State = State.Deleted,
+                PublishedAt = DateTime.Now.AddMonths(-6),
+                Title = "How to use ...."
+            };
+
+            var listOfPosts = new List<BlogPost>();
+            listOfPosts.Add(post);
+            listOfPosts.Add(post2);
+            listOfPosts.Add(post3);
+
+            var jsonList = JsonConvert.SerializeObject(listOfPosts, Formatting.Indented);
+
+            Console.WriteLine(jsonList);
+
+
+            //var deserializedListWrong = JsonConvert.DeserializeObject<BlogPost>(jsonList);
+
+
+            var deserializedList = JsonConvert.DeserializeObject<List<BlogPost>>(jsonList);
+
+            var defaultSerialisation = JsonConvert.SerializeObject(post2);
+            Console.WriteLine(defaultSerialisation);
+
+
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                NullValueHandling = NullValueHandling.Ignore,
+                DateFormatHandling = DateFormatHandling.MicrosoftDateFormat
+            };
+
+            var postWithCamelCase = JsonConvert.SerializeObject(post3, settings);
+            Console.WriteLine(postWithCamelCase);
+
+           
+        }
+    }
+
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime? PublishedAt { get; set; }
+        public State State { get; set; }
+        public List<string> Tags { get; set; }
+
+        public BlogPost()
+        {
+            Tags = new List<string>();
+        }
+    }
+
+    public enum State
+    {
+        Draft,
+        Published,
+        Deleted,
+    }
+}
