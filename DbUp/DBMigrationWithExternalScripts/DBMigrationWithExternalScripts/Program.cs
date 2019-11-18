62e03de0b2796abbb37f7102c91983c7bbcc736c
Johnny Graber <Johnny.Graber@fmh.ch>
2019-11-18 20:19:41 +0100

    add example for DbUp with external scripts

---
+++ DbUp/DBMigrationWithExternalScripts/DBMigrationWithExternalScripts/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using DbUp;
+
+namespace DBMigrationWithExternalScripts
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var connectionString =
+                args.FirstOrDefault()
+                ?? "Data Source=.;Initial Catalog=BlogSnippets;Integrated Security=True;MultipleActiveResultSets=True";
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsFromFileSystem(@"../../../../Scripts/")
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
