3b70b26503e6d4aff91ac0ff92d4a105d70e08cd
Johnny Graber <Johnny.Graber@fmh.ch>
2020-09-23 21:49:35 +0200

    CatchAllRule: Add EverythingController and view

---
+++ CatchAllRule/CatchAllRule/Controllers/EverythingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CatchAllRule.Controllers
+{
+    public class EverythingController : Controller
+    {
+        // GET: Everything
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
