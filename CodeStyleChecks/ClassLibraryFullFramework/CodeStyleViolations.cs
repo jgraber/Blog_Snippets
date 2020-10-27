2b28725fffeeef7e2b144df3e86672bfbbf304a4
Johnny Graber <jg@jgraber.ch>
2020-10-27 23:15:55 +0100

    Start with example on code style checker

---
+++ CodeStyleChecks/ClassLibraryFullFramework/CodeStyleViolations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeStyleChecks
+{
+    public class CodeStyleViolations
+    {
+        public string name { get; set; }
+
+        public CodeStyleViolations()
+        {
+
+        }
+
+
+
+        public bool Works()
+        {
+            var a = "a string";
+            var b = true;
+
+            if (b)
+            {
+                Console.WriteLine("Hello");
+            }
+            else
+            {
+                Console.WriteLine("Bye");
+            }
+
+            return b;
+        }
+    }
+}
---
+++ CodeStyleChecks/CodeStyleChecks/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CodeStyleChecks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var x = new CodeStyleViolations();
+            x.Works();
+        }
+    }
+}
