4fc331efec8a3a1bf21cd58130f923c789048c45
Johnny Graber <jg@jgraber.ch>
2021-02-28 16:00:00 +0100

    StyleCop & Net5: add a test project

---
+++ Stylecop/StyleCopNet5.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace StyleCopNet5.Domain.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
