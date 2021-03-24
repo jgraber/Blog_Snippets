32e750cea676e7a67016342d72d2dca24e6b2cd2
Johnny Graber <jg@jgraber.ch>
2021-03-25 00:05:02 +0100

    add project

---
+++ CodeGenerationPercentage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeGenerationPercentage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generated Code Percentage Calculator");
+        }
+    }
+}
