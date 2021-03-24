f019dcdb5c97374263b95cf454e0476ab2842426
Johnny Graber <jg@jgraber.ch>
2021-03-25 00:37:09 +0100

    add example data

---
+++ CodeGenerationPercentage.Tests/DemoProject/modulD/subModulD2/Class7.g.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGenerationPercentage.Tests.DemoProject.modulD.subModulD2
+{
+    class Class7
+    {
+    }
+}
