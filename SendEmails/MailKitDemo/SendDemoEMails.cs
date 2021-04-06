7465a298358bea1671f1a9f1373bba83463d905e
Johnny Graber <jg@jgraber.ch>
2021-04-06 20:01:28 +0200

    add example for Papercut

--- SendEmails/MailKitDemo/SendDemoEMails.cs
+++ SendEmails/MailKitDemo/SendDemoEMails.cs
@@ -1,3 +1,7 @@
+using System;
+using System.IO;
+using System.Threading;
+
 using MailKit.Net.Smtp;
 
 using MimeKit;
@@ -19,7 +23,7 @@ namespace MailKitDemo
         /// * Run smtp4dev: smtp4dev
         /// </summary>
         [Test]
-        public void SendEMail()
+        public void SendEMailToSmtp4dev()
         {
             var message = new MimeMessage();
             message.From.Add(new MailboxAddress("Joey Tribbiani", "joey@friends.com"));
@@ -44,6 +48,55 @@ I just wanted to let you know that Monica and I were going to go play some paint
 
                 client.Send(message);
                 client.Disconnect(true);
+            }
+        }
+
+        /// <summary>
+        /// Run demo for mailing and see what shows up in Papercut (https://github.com/ChangemakerStudios/Papercut-SMTP)
+        /// * Install Papercut via Installer : https://github.com/ChangemakerStudios/Papercut-SMTP/releases (use exe in current release)
+        /// * Run Papercut in System menu (make sure that no other SMTP Server runs)
+        /// </summary>
+        [Test]
+        public void SendEMailToPapercut()
+        {
+            var id = Guid.NewGuid();
+            var subject = $"Test #{id.ToString().Substring(0,6)}";
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Joey Tribbiani", "joey@friends.com"));
+            message.To.Add(new MailboxAddress("Mrs. Chanandler Bong", "chandler@friends.com"));
+            message.Subject = subject;
+
+            message.Body = new TextPart("plain")
+                               {
+                                   Text = @"Hey Chandler,
+
+I just wanted to let you know that Monica and I were going to go play some paintball, you in?
+
+-- Joey"
+                               };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("localhost", 25, false);
+
+                // Note: only needed if the SMTP server requires authentication
+                //client.Authenticate("joey", "password");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
+
+            var pathToStorage = @"C:\Users\jg\AppData\Roaming\Changemaker Studios\Papercut SMTP\";
+            var currentDate = DateTime.Now.ToString("yyyyMMddhhmm");
+
+            Thread.Sleep(2000);
+            DirectoryInfo emailDir = new DirectoryInfo(pathToStorage);
+            FileInfo[] taskFiles = emailDir.GetFiles($"*{subject}*.eml");
+
+            foreach (var file in taskFiles)
+            {
+                Console.WriteLine(file.FullName);
             }
         }
     }
