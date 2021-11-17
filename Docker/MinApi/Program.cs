15574326f973d471229c9d3e6404d209f7f55b48
Johnny Graber <jg@jgraber.ch>
2021-11-17 18:57:27 +0100

    connect to database

--- Docker/MinApi/Program.cs
+++ Docker/MinApi/Program.cs
@@ -1,17 +1,43 @@
 using Dapper;
 using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var config = app.Services.GetRequiredService<IConfiguration>();
+var connectionString = config.GetConnectionString("Adventure");
+Console.WriteLine(connectionString);
+var sqlCon = new SqlConnection(connectionString);
+var emplyeeHandler = new EmployeeHandler(sqlCon);
 
-app.MapGet("/{name}", (string name) => HelloHandler.Hello(name));
+app.MapGet("/", () => "Welcome!");
 
-app.Run();
+app.MapGet("/employee/{id}", (int id) => emplyeeHandler.FindById(id));
 
-class HelloHandler
+app.Run("http://+:7000");
+
+class EmployeeHandler
 {
-    public static string Hello(string name = "static method")
+    private readonly SqlConnection connection;
+
+    public EmployeeHandler(SqlConnection connection)
     {
-        return $"Hello {name}";
+        this.connection = connection;
+    }
+
+    public Person FindById(int id)
+    {
+        var query = @"SELECT [BusinessEntityID] AS [Id]
+                          ,[FirstName]
+                          ,[LastName]
+                          ,[ModifiedDate]
+                      FROM [AdventureWorks2019].[Person].[Person]
+                      WHERE BusinessEntityID = @id";
+
+        var result = connection.Query<Person>(query, new { id }).First();
+        
+        return result;
     }
 }
+
+public record Person(int Id, string FirstName, string LastName, DateTime ModifiedDate);
