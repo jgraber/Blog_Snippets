64972fe042f81791ec1242d4481163a32f81ca96
Johnny Graber <jg@jgraber.ch>
2021-12-01 23:13:46 +0100

    add example for ConfigurationBuilder()

---
+++ Configuration/ConfigBuilderDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace ConfigBuilderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings_1.json", true, true)
+                .AddJsonFile("appsettings_2.json", true, true);
+            //.AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "..", "appsettings.json"), true, true)
+            //.AddEnvironmentVariables();
+
+            var configuration = builder.Build();
+
+            Console.WriteLine(configuration["AppSettings:myKey"]);
+        }
+    }
+}
