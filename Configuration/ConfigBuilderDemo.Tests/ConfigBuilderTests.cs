353c68050598ad97d67d00647a5ede9765665fd3
Johnny Graber <jg@jgraber.ch>
2021-12-02 20:10:13 +0100

    add test to check difference of order in ConfigBuilder

---
+++ Configuration/ConfigBuilderDemo.Tests/ConfigBuilderTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+
+namespace ConfigBuilderDemo.Tests
+{
+    public class ConfigBuilderTests
+    {
+        [Test]
+        public void AllConfigurationFilesCanBeAccessedThroughConfigBuilder()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings_1.json", true, true)
+                .AddJsonFile("appsettings_2.json", true, true); // <== wins for keys that are in both files
+            var config = builder.Build();
+
+            Assert.AreEqual("user", config["AppSettings:onlyInFirst"]);
+            Assert.AreEqual("car", config["AppSettings:onlyInSecond"]);
+            Assert.AreEqual("2", config["AppSettings:myKey"]);
+        }
+
+        [Test]
+        public void OrderOfBuilderConfigMatters()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings_2.json", true, true)
+                .AddJsonFile("appsettings_1.json", true, true); // <== wins for keys that are in both files
+            var config = builder.Build();
+
+            Assert.AreEqual("1", config["AppSettings:myKey"]);
+        }
+    }
+}
