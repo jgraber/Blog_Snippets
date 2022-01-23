0aea2746a9f04fd314f1165c8832a064ca9d8de1
Johnny Graber <jg@jgraber.ch>
2022-01-23 20:55:00 +0100

    add example for sequence

---
+++ Enumerable/GetSequence/GetSequence/Program.cs
@@ -0,0 +1,17 @@
+﻿IEnumerable<int> numbers = Enumerable.Range(1, 10);
+
+foreach (int num in numbers)
+{
+    Console.WriteLine(num);
+}
+
+
+Console.WriteLine("\n\n\n");
+
+
+IEnumerable<int> squares = Enumerable.Range(1, 10).Select(x => x * x);
+
+foreach (int num in squares)
+{
+    Console.WriteLine(num);
+}
