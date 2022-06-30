df1315aa066b1c77748838cada8fddc6565d4099
Johnny Graber <jg@jgraber.ch>
2022-06-30 21:40:23 +0200

    start with split phase refactoring

---
+++ Refactoring/BirthdayCalendar/BirthdayCalendarCreator.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace BirthdayCalendar
+{
+    public class BirthdayCalendarCreator
+    {
+        private readonly string _connectionString;
+
+        public BirthdayCalendarCreator(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public string Create()
+        {
+            var connection = new SqlConnection(_connectionString);
+
+            // Load data
+            var sql = @"SELECT Title, FirstName, MiddleName, LastName, e.BirthDate  
+                        FROM Person.Person p
+                        INNER JOIN HumanResources.Employee e 
+	                        ON e.BusinessEntityID = p.BusinessEntityID
+                        ORDER BY MONTH(e.BirthDate), DAY(e.BirthDate)";
+            
+            var rows = connection.Query(sql);
+
+
+            // Process data
+            var report = new StringBuilder();
+            foreach (var row in rows) 
+            {
+                // Cleanup
+                DateTime birthday = ((DateTime)row.BirthDate).Date;
+                string middleName = (string)row.MiddleName;
+                if (!string.IsNullOrEmpty(middleName) && middleName.Length == 1)
+                {
+                    middleName = $"{middleName}.";
+                }
+                string title = (string)row.Title;
+                if (string.IsNullOrEmpty(title))
+                {
+                    title = "      ";
+                }
+                else
+                {
+                    title = $"({title}) ";
+                }
+
+                // Print report
+                report.Append($"{title} {row.FirstName} {middleName} {row.LastName}: {birthday.ToShortDateString()}\n");
+            }
+
+            
+            return report.ToString();
+        }
+    }
+}
---
+++ Refactoring/BirthdayCalendar/Program.cs
@@ -0,0 +1,7 @@
+﻿using BirthdayCalendar;
+
+var connection = "Server=.;Database=AdventureWorks2019;Integrated Security=true;";
+var calendar = new BirthdayCalendarCreator(connection);
+var report = calendar.Create();
+
+Console.WriteLine(report);
