8d92e676cf1f7421aa70f278bb11117202525116
Johnny Graber <jg@jgraber.ch>
2022-11-01 22:52:00 +0100

    add Playwright to a CLI tool

---
+++ Playwright/PlaywrightDemo.Library/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static async Task Main()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(
+                                      new BrowserTypeLaunchOptions
+                                          {
+                                              Headless = false,
+                                          });
+        var context = await browser.NewContextAsync();
+
+        var Page = await context.NewPageAsync();
+
+        await Page.GotoAsync("https://improveandrepeat.com/");
+
+    }
+}
