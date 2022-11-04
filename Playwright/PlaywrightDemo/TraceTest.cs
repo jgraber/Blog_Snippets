cf8ce362f612186800c3572b7dae8331a37bc653
Johnny Graber <jg@jgraber.ch>
2022-11-04 23:42:00 +0100

    use TraceIt category in tests

--- Playwright/PlaywrightDemo/TraceTest.cs
+++ Playwright/PlaywrightDemo/TraceTest.cs
@@ -41,4 +41,35 @@ public class TraceTest : PageTest
         }
     }
 
+    [Test]
+    [Category("TraceIt")]
+    public async Task LinkingToIntro()
+    {
+        await Page.GotoAsync("https://playwright.dev");
+
+        // Expect a title "to contain" a substring.
+        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+
+        // create a locator
+        var getStarted = Page.Locator("text=Get Started");
+
+        // Expect an attribute "to be strictly equal" to the value.
+        await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
+
+        // Click the get started link.
+        await getStarted.ClickAsync();
+
+        // Expects the URL to contain intro.
+        await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
+    }
+
+    [Test]
+    [Category("TraceIt")]
+    public async Task MissingText()
+    {
+        await Page.GotoAsync("https://playwright.dev");
+
+        // Expect a title "to contain" a substring.
+        await Expect(Page).ToHaveTitleAsync(new Regex("PlaywrightXYZ"));
+    }
 }
