da8016ec7b963ecbcf250b2831d44c266ff1bf0b
Johnny Graber <jg@jgraber.ch>
2022-11-19 22:44:00 +0100

    add dotenv.net package

--- Playwright/PlaywrightDemo.Connect/Program.cs
+++ Playwright/PlaywrightDemo.Connect/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using dotenv.net;
+
 Console.WriteLine("Hello, World!");
+
+DotEnv.Load();
+var envVars = DotEnv.Read();
+
+Console.WriteLine(envVars["browserstack.username"]);
