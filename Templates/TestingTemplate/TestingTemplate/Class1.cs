cf4a426b71b5edfcff7151c736a3f4c11369ba6e
Johnny Graber <jg@jgraber.ch>
2023-07-24 12:58:13 +0200

    add dependencies

--- Templates/TestingTemplate/TestingTemplate/Class1.cs
+++ Templates/TestingTemplate/TestingTemplate/Class1.cs
@@ -1,7 +1,14 @@
-﻿namespace TestingTemplate
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace TestingTemplate
 {
     public class Class1
     {
-
+        [Test]
+        public void X()
+        {
+            "Hello".Should().BeEquivalentTo("hello");
+        }
     }
 }
