a78ba36c9445461e766b294ba2b081c3374a130e
Johnny Graber <jg@jgraber.ch>
2023-10-25 20:58:29 +0200

    add minimalistic example to create QR codes

---
+++ QrCodes/QrCodeDemo/CreateQrCodesTests.cs
@@ -0,0 +1,15 @@
+﻿using Net.Codecrete.QrCodeGenerator;
+using NUnit.Framework;
+
+namespace QrCodeDemo
+{
+    public class CreateQrCodesTests
+    {
+        [Test]
+        public void CreatePngQrCode()
+        {
+            var qr = QrCode.EncodeText("https://improveandrepeat.com/", QrCode.Ecc.Medium);
+            qr.SaveAsPng("hello-world-qr.png", 10, 3);
+        }
+    }
+}
