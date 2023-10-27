01f85c969a6f754ef5a9eed2c98fcd02d68347eb
Johnny Graber <jg@jgraber.ch>
2023-10-27 16:15:13 +0200

    add example for SVG QR codes

--- QrCodes/QrCodeDemo/CreateQrCodesTests.cs
+++ QrCodes/QrCodeDemo/CreateQrCodesTests.cs
@@ -19,6 +19,13 @@ namespace QrCodeDemo
             var pngAsBytes = qr.ToPng( 10, 3);
             File.WriteAllBytes("qr-url-bytes.png", pngAsBytes);
         }
+
+        [Test]
+        public void CreateSvgQrCode()
+        {
+            var qr = QrCode.EncodeText("https://improveandrepeat.com/", QrCode.Ecc.High);
+            var svgAsString = qr.ToSvgString(5);
+            File.WriteAllText("qr-url.svg", svgAsString);
         }
     }
 }
