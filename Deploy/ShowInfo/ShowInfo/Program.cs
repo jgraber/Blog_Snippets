32491b0c93b5783accf819037def460a02aa13e1
Johnny Graber <jg@jgraber.ch>
2024-02-10 23:11:26 +0100

    add example for dotnet deploy

---
+++ Deploy/ShowInfo/ShowInfo/Program.cs
@@ -0,0 +1,7 @@
+﻿// old cmd: dotnet publish --self-contained --runtime ubuntu.20.04-x64 =>  See https://aka.ms/netsdk1083
+// new cmd: dotnet publish --self-contained --runtime linux-x64
+
+Console.WriteLine($".Net version: \t{Environment.Version}");
+Console.WriteLine($"OS version: \t{Environment.OSVersion}");
+Console.WriteLine($"Machine name: \t{Environment.MachineName}");
+Console.WriteLine($"Command: \t{Environment.CommandLine}");
