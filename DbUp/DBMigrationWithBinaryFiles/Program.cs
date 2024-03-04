fcb548c99379c9d379b7b41f6b20dd65c512e465
Johnny Graber <jg@jgraber.ch>
2024-03-04 20:59:32 +0100

    start example for DbUp and binary files

---
+++ DbUp/DBMigrationWithBinaryFiles/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using DbUp;
+
+namespace WithBinary
+{
+    public class Program
+    {
+        static int Main(string[] args)
+        {
+            var connectionString = "Data Source=.;Initial Catalog=Demo;Integrated Security=True;MultipleActiveResultSets=True;Encrypt=False;";
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .WithTransactionPerScript()
+                    .WithVariablesDisabled()
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
