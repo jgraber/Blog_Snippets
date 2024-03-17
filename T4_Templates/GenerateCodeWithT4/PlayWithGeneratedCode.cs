88eb6e229e207a0eee3bdd0690fab937973da235
Johnny Graber <jg@jgraber.ch>
2024-03-17 22:52:50 +0100

    add a basic example for T4

---
+++ T4_Templates/GenerateCodeWithT4/PlayWithGeneratedCode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.TestSubFolder;
+
+namespace GenerateCodeWithT4
+{
+    internal class PlayWithGeneratedCode
+    {
+        public void Works()
+        {
+            var product = new ProductDto();
+            var order = new OrderDto();
+            order.Id = 1;
+        }
+    }
+}
