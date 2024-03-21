c422a3331cb543788f2dae0370946281d5677eb8
Johnny Graber <jg@jgraber.ch>
2024-03-22 00:11:29 +0100

    add example to generate entities from a database

---
+++ T4_Templates/GenerateCodeFromDb/FromDb/Product.g.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Test.FromDb
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string Description { get; set; }
+    }
+}
+
