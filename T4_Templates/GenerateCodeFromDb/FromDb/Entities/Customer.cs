ff9501796404ed6dd9b508b7511c9b3e19ec41ee
Johnny Graber <jg@jgraber.ch>
2024-04-08 21:53:20 +0200

    extend the generated entity Customer with custom logic

---
+++ T4_Templates/GenerateCodeFromDb/FromDb/Entities/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.FromDb.Entities
+{
+    public partial class Customer
+    {
+        public string GetAddress()
+        {
+            var address = $"{FirstName} {LastName}" +
+                          $"\n{StreetAndNumber}" +
+                          $"\n{City}, {State} {ZipCode}" +
+                          $"\n{Country(CountryCode)}";
+
+            return address;
+        }
+
+        private string Country(string code)
+        {
+            if (code.Equals("US"))
+            {
+                return "United States";
+            }
+            else
+            {
+                return code;
+            }
+        }
+    }
+}
---
+++ T4_Templates/GenerateCodeFromDb/FromDb/UnitTests/CustomerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using Test.FromDb.Entities;
+
+namespace GenerateCodeFromDb.FromDb.UnitTests
+{
+    [TestFixture]
+    public partial class CustomerTests
+    {
+        [Test]
+        public void GetAddress_formats_US_address()
+        {
+            var customer = new Customer
+            {
+                LastName = "Example",
+                FirstName = "Joe",
+                CreatedOn = new DateTime(2024, 4, 1, 12, 30, 45),
+                Email = "test@example.com",
+                IsActive = true,
+                StreetAndNumber = "One Microsoft Way",
+                City = "Redmond",
+                ZipCode = "98052",
+                State = "WA",
+                CountryCode = "US"
+            };
+
+            var formattedAddress = customer.GetAddress();
+
+            formattedAddress.Should().Be("Joe Example" +
+                                         "\nOne Microsoft Way" +
+                                         "\nRedmond, WA 98052" +
+                                         "\nUnited States");
+        }
+    }
+}
