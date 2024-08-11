6e8a99b9afa5e379c103eeb1a1a2549aafb72b67
Johnny Graber <jg@jgraber.ch>
2024-08-11 22:03:55 +0200

    add example for NBomber

---
+++ Benchmarks/NBomberDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using NBomber.CSharp;
+
+namespace MyLoadTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var httpClient = new HttpClient();
+
+            var scenario = Scenario.Create("hello_world_scenario", async context =>
+                {
+                    var response = await httpClient.GetAsync("https://jgraber.com/");
+
+                    return response.IsSuccessStatusCode
+                        ? Response.Ok()
+                        : Response.Fail();
+                })
+                .WithoutWarmUp()
+                .WithLoadSimulations(
+                    Simulation.Inject(rate: 10,
+                        interval: TimeSpan.FromSeconds(1),
+                        during: TimeSpan.FromSeconds(30))
+                );
+
+            NBomberRunner
+                .RegisterScenarios(scenario)
+                .Run();
+        }
+    }
+}
