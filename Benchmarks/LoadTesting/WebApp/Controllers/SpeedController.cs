4a759efb83129b8cd214a7e3af2e144f39acf7f2
Johnny Graber <jg@jgraber.ch>
2024-08-17 16:24:14 +0200

    add SpeedController

---
+++ Benchmarks/LoadTesting/WebApp/Controllers/SpeedController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class SpeedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Empty()
+        {
+            return View();
+        }
+    }
+}
