aaa111e6d4b41374e144dd8610eebe1987648a64
Johnny Graber <jg@jgraber.ch>
2024-08-27 22:19:02 +0200

    add tests for slow/asyncSlow endpoints

--- Benchmarks/LoadTesting/NBomberDemo/Program.cs
+++ Benchmarks/LoadTesting/NBomberDemo/Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Threading.Tasks;
-using System.Net.Http;
+﻿using System.Net;
 using NBomber.CSharp;
+using NBomber.Http;
 
 namespace MyLoadTest
 {
@@ -11,23 +10,53 @@ namespace MyLoadTest
         {
             using var httpClient = new HttpClient();
 
-            var scenario = Scenario.Create("hello_world_scenario", async context =>
+            var slow = Scenario.Create("Slow", async context =>
                 {
-                    var response = await httpClient.GetAsync("https://localhost:7285/");
+                    var response = await httpClient.GetAsync("https://localhost:7285/Speed/Slow");
 
                     return response.IsSuccessStatusCode
-                        ? Response.Ok()
-                        : Response.Fail();
+                        ? Response.Ok<string>(statusCode: response.StatusCode.ToString())
+                        : Response.Fail<string>(statusCode: response.StatusCode.ToString());
                 })
-                .WithoutWarmUp()
+                .WithWarmUpDuration(TimeSpan.FromSeconds(5))
                 .WithLoadSimulations(
-                    Simulation.Inject(rate: 1_000,
+                    Simulation.RampingInject(rate: 1000,
                         interval: TimeSpan.FromSeconds(1),
-                        during: TimeSpan.FromSeconds(30))
+                        during: TimeSpan.FromSeconds(20)),
+                    Simulation.Inject(rate: 1000,
+                        interval: TimeSpan.FromSeconds(1),
+                        during: TimeSpan.FromSeconds(120)),
+                    Simulation.RampingInject(rate: 0,
+                        interval: TimeSpan.FromSeconds(1),
+                        during: TimeSpan.FromSeconds(10))
+                );
+
+            var asyncSlow = Scenario.Create("AsyncSlow", async context =>
+                {
+                    var response = await httpClient.GetAsync("https://localhost:7285/Speed/AsyncSlow");
+
+                    return response.IsSuccessStatusCode
+                        ? Response.Ok<string>(statusCode:response.StatusCode.ToString())
+                        : Response.Fail<string>(statusCode: response.StatusCode.ToString());
+                })
+                .WithWarmUpDuration(TimeSpan.FromSeconds(5))
+                .WithLoadSimulations(
+                    Simulation.RampingInject(rate: 1000,
+                        interval: TimeSpan.FromSeconds(1),
+                        during: TimeSpan.FromSeconds(20)),
+                    Simulation.Inject(rate: 1000,
+                        interval: TimeSpan.FromSeconds(1),
+                        during: TimeSpan.FromSeconds(120)),
+                    Simulation.RampingInject(rate: 0,
+                        interval: TimeSpan.FromSeconds(1),
+                        during: TimeSpan.FromSeconds(10))
                 );
 
             NBomberRunner
-                .RegisterScenarios(scenario)
+                .RegisterScenarios(asyncSlow)
+                .WithWorkerPlugins(new HttpMetricsPlugin(new[] { NBomber.Http.HttpVersion.Version1 }))
+                //.WithReportFormats(ReportFormat.Html)
+                //.WithReportFolder("Reports")
                 .Run();
         }
     }
