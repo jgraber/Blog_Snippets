b796b240e154e46dcdc0161a8888a4baef56e829
Johnny Graber <jg@jgraber.ch>
2024-09-02 21:44:45 +0200

    add slow method to load test

--- Benchmarks/LoadTesting/WebApp/Controllers/SpeedController.cs
+++ Benchmarks/LoadTesting/WebApp/Controllers/SpeedController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Runtime.Serialization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebApp.Controllers
 {
@@ -24,6 +26,25 @@ namespace WebApp.Controllers
         {
             await Task.Delay(50);
             return View();
+        }
+
+        public IActionResult Data()
+        {
+            var data = GetRandomNumbersAsync(10);
+            return View(data);
+        }
+
+        private static List<int> GetRandomNumbersAsync(int count)
+        {
+            Random _random = new Random();
+            var randomNumbers = new List<int>();
+            for (int i = 0; i < count; i++)
+            {
+                Thread.Sleep(100);
+                randomNumbers.Add(_random.Next());
+            }
+
+            return randomNumbers;
         }
     }
 }
