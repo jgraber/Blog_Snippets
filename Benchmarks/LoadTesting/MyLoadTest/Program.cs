9bbbe9cf621811f6c0fe8b2c5fd3d4a326f306aa
Johnny Graber <jg@jgraber.ch>
2025-02-05 06:20:05 +0100

    add minimal NBomber example

---
+++ Benchmarks/LoadTesting/MyLoadTest/Program.cs
@@ -0,0 +1,55 @@
+﻿using NBomber.CSharp;
+using NBomber.Http.CSharp;
+
+using var httpClient = new HttpClient();
+
+var scenario = Scenario.Create("http_scenario", async context =>
+{
+    var request =
+        Http.CreateRequest("GET", "https://nbomber.com")
+            .WithHeader("Accept", "text/html");
+            
+
+    var response = await Http.Send(httpClient, request);
+
+    return response;
+})
+.WithoutWarmUp()
+.WithLoadSimulations(
+    Simulation.Inject(rate: 100,
+                        interval: TimeSpan.FromSeconds(1),
+                        during: TimeSpan.FromSeconds(30))
+);
+//.WithLoadSimulations(
+//    Simulation.RampingInject(100,
+//        interval: TimeSpan.FromSeconds(1),
+//        during: TimeSpan.FromSeconds(20)),
+//    Simulation.Inject(rate: 100,
+//                        interval: TimeSpan.FromSeconds(1),
+//                        during: TimeSpan.FromSeconds(20)),
+//    Simulation.RampingInject(50,
+//        interval: TimeSpan.FromSeconds(1),
+//        during: TimeSpan.FromSeconds(20))
+//);
+
+var docs = Scenario.Create("docs_scenario", async context =>
+{
+    var request =
+        Http.CreateRequest("GET", "https://nbomber.com/docs/getting-started/overview/")
+            .WithHeader("Accept", "text/html");
+
+
+    var response = await Http.Send(httpClient, request);
+
+    return response;
+})
+.WithoutWarmUp()
+.WithLoadSimulations(
+    Simulation.Inject(rate: 100,
+                        interval: TimeSpan.FromSeconds(1),
+                        during: TimeSpan.FromSeconds(30))
+);
+
+NBomberRunner
+    .RegisterScenarios(scenario, docs)
+    .Run();
