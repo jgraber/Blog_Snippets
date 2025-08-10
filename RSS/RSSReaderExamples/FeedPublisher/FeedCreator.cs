4c7807dce0aa6304fcb9ebcab0813c983d369392
Johnny Graber <jg@jgraber.ch>
2025-08-10 14:57:52 +0200

    optimisations for blog post

--- RSS/RSSReaderExamples/FeedPublisher/FeedCreator.cs
+++ RSS/RSSReaderExamples/FeedPublisher/FeedCreator.cs
@@ -11,7 +11,7 @@ namespace RSSProducer
             // Example from https://learn.microsoft.com/en-us/dotnet/framework/wcf/feature-details/how-to-create-a-basic-rss-feed
 
             SyndicationFeed feed = new SyndicationFeed("My Blog Feed", "This is a test feed", new Uri("http://SomeURI"));
-            feed.Authors.Add(new SyndicationPerson("jg@jgraber.ch", "Johnny Graber", "https://improveandrepeat.com/"));
+            feed.Authors.Add(new SyndicationPerson("info@improveandrepeat.com", "Johnny Graber", "https://improveandrepeat.com/"));
             feed.Categories.Add(new SyndicationCategory(".Net"));
             feed.Description = new TextSyndicationContent("Basic example to produce a RSS feed");
             feed.ImageUrl = new Uri("https://jgraber.ch/images/background.jpg");
@@ -22,8 +22,8 @@ namespace RSSProducer
                 "This is the content for item one",
                 new Uri("http://localhost/Content/One"),
                 "ItemOneID",
-                DateTimeOffset.Now.AddDays(1));
-            item1.PublishDate = DateTimeOffset.Now;
+                DateTimeOffset.Now);
+            item1.PublishDate = DateTimeOffset.Now.AddDays(-3);
             item1.Authors.Add(new SyndicationPerson("A.A@...."));
             item1.Categories.Add(new SyndicationCategory("AI"));
 
@@ -32,8 +32,8 @@ namespace RSSProducer
                 "This is the content for item two",
                 new Uri("http://localhost/Content/Two"),
                 "ItemTwoID",
-                DateTimeOffset.Now.AddDays(1));
-            item2.PublishDate = DateTimeOffset.Now;
+                DateTimeOffset.Now);
+            item2.PublishDate = DateTimeOffset.Now.AddDays(-2);
             item2.Authors.Add(new SyndicationPerson("B.B@...."));
             item2.Categories.Add(new SyndicationCategory("Python"));
 
@@ -42,8 +42,8 @@ namespace RSSProducer
                 "This is the content for item three",
                 new Uri("http://localhost/Content/three"),
                 "ItemThreeID",
-                DateTimeOffset.Now.AddDays(1));
-            item3.PublishDate = DateTimeOffset.Now;
+                DateTimeOffset.Now);
+            item3.PublishDate = DateTimeOffset.Now.AddHours(-1);
             item3.Authors.Add(new SyndicationPerson("C.C@...."));
             item3.Categories.Add(new SyndicationCategory(".Net"));
             item3.Categories.Add(new SyndicationCategory("Practice"));
